eef657a86def6c9b819fcce2f39f10ff6518204e
Steve Weis <steve@privatecore.com>
2014-07-08 11:12:39 -0700

    Adding method

--- sentencegen.py
+++ sentencegen.py
@@ -5,8 +5,15 @@ Generate random sentences
 import math
 import random  # For demo only.
 
-elements = [ "adjective", "adverb", "noun",
-             "preposition", "pronoun", "verb"]
+
+def read_word_sources():
+    '''Reads a set of files named [element]s.txt in the same directory'''
+    filenames = [ "adjective", "adverb", "noun",
+                  "preposition", "pronoun", "verb"]
+    elem_map = {}
+    for f in filenames:
+        elem_map[f] = [l.strip() for l in open("{}s.txt".format(f)).readlines()]
+    return elem_map
 
 def measure_entropy(template, element_map):
     return int(sum(map(lambda x: math.log(len(element_map[x]), 2), template)))
@@ -14,16 +21,14 @@ def measure_entropy(template, element_map):
 def generate_sentence(template, element_map):
     return map(lambda x: random.sample(element_map[x], 1)[0], template)
 
-elem_map = {}
-for e in elements:
-    # This is expecting files named [element]s.txt in the same directory
-    elem_map[e] = [l.strip() for l in open("{}s.txt".format(e)).readlines()]
 
 example_template = [ "preposition", "adjective", "noun", "adverb",
                      "adjective", "noun"]
 
+wordlists = read_word_sources()
+
 print "Example template:", " ".join(example_template)
-print "Entropy:", measure_entropy(example_template, elem_map)
+print "Entropy:", measure_entropy(example_template, wordlists)
 print "Samples:"
 for i in xrange(10):
-    print "\t", " ".join(generate_sentence(example_template, elem_map))
+    print "\t", " ".join(generate_sentence(example_template, wordlists))
